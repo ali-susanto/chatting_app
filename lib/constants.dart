820a22ef674353208f7bb755fa7276bedb8709c1
ali-susanto <alisusanto.main@gmail.com>
2022-07-28 19:38:57 +0700

    add constants color & style text

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF794B93);
+const kSecondaryColor = Color.fromARGB(255, 51, 46, 46);
+
+const kTitle = TextStyle(
+    color: kPrimaryColor,
+    fontFamily: 'Nunito',
+    fontSize: 17.0,
+    height: 1.5,
+    fontWeight: FontWeight.bold);
+
+const kSubtitle = TextStyle(
+  color: kSecondaryColor,
+  fontFamily: 'Nunito',
+  fontSize: 15.0,
+  height: 1.2,
+);
