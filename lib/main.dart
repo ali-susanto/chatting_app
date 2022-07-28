eca2407452103a414d6aad118b243d3a3b4f6053
ali-susanto <alisusanto.main@gmail.com>
2022-07-28 19:39:38 +0700

    change default font to nunito

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chatting_app/screen/splash/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'chatting',
       theme: ThemeData(
+        textTheme: GoogleFonts.nunitoTextTheme(Theme.of(context).textTheme),
         primarySwatch: Colors.blue,
       ),
       routes: {'/splash': (context) => const SplashScreen()},
